b87c6db8ea261ad7e0b0441aeb466a17957b763e
TMarwah <44328801+TMarwah@users.noreply.github.com>
2020-05-10 13:45:05 -0700

    started work on turtle game

--- turtle_game.py
+++ turtle_game.py
@@ -1,1 +1,29 @@
 import turtle
+import random
+from turtle import *
+
+def up():
+    turtle.setheading(90)
+    turtle.forward(100)
+
+def down():
+    turtle.setheading(270)
+    turtle.forward(100)
+
+def left():
+    turtle.setheading(180)
+    turtle.forward(100)
+
+def right():
+    turtle.setheading(0)
+    turtle.forward(100)
+
+turtle.listen()
+
+turtle.onkey(up(),'Up')
+turtle.onkey(down(),'Down')
+turtle.onkey(right(),'Right')
+turtle.onkey(left(),'Left')
+
+turtle.mainloop()
+
