22bc7e101452afa0ab3925c44aa68b631a929d1a
TMarwah <44328801+TMarwah@users.noreply.github.com>
2020-05-10 13:11:10 -0700

    Created Beaded Bracelet

--- sidewalk.py
+++ sidewalk.py
@@ -1,5 +1,13 @@
 import turtle
-turtle.forward(100)
-turtle.forward(500)
-turtle.right(90)
+turtle.speed(100)
+for i in range(36):
+    turtle.penup()
+    turtle.forward(110)
+    turtle.pendown()
+    turtle.circle(10)
+    turtle.penup()
+    turtle.backward(110)
+    turtle.left(10)
+
+
 turtle.Screen().exitonclick()
