a62bf27002abda5e9b0b6ed0a6a29bed9f2e6fc3
TMarwah <44328801+TMarwah@users.noreply.github.com>
2020-05-10 13:18:19 -0700

    split red/white/blue into seperate functions, fix fill

--- beaded_bracelet.py
+++ beaded_bracelet.py
@@ -1,13 +1,44 @@
 import turtle
 turtle.speed(100)
-for i in range(36):
+def red_set():
     turtle.penup()
     turtle.forward(110)
     turtle.pendown()
+    turtle.begin_fill()
+    turtle.fillcolor("red")
     turtle.circle(10)
+    turtle.end_fill()
     turtle.penup()
     turtle.backward(110)
     turtle.left(10)
 
+def white_set():
+    turtle.penup()
+    turtle.forward(110)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.fillcolor("white")
+    turtle.circle(10)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.backward(110)
+    turtle.left(10)
+
+def blue_set():
+    turtle.penup()
+    turtle.forward(110)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.fillcolor("blue")
+    turtle.circle(10)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.backward(110)
+    turtle.left(10)
+
+for i in range(12):
+    red_set()
+    white_set()
+    blue_set()
 
 turtle.Screen().exitonclick()
