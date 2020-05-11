465fb2d3b5097f5114140e10515180e210760bbf
TMarwah <44328801+TMarwah@users.noreply.github.com>
2020-05-10 18:13:04 -0700

    modified angles

--- turtle_game.py
+++ turtle_game.py
@@ -2,27 +2,23 @@ import turtle
 
 from turtle import Turtle, Screen
 screen = Screen()
-screen.setup(1200,500)
+screen.setup(500,500)
 def up():
-    turtle.setheading(90)
     turtle.forward(100)
 
-def down():
-    turtle.setheading(270)
-    turtle.forward(100)
+def back():
+    turtle.backward(100)
 
 def left():
-    turtle.setheading(180)
-    turtle.forward(100)
+    turtle.left(40)
 
 def right():
-    turtle.setheading(0)
-    turtle.forward(100)
+    turtle.right(40)
 
 
 
 turtle.Screen().onkeypress(up,'Up')
-turtle.Screen().onkeypress(down,'Down')
+turtle.Screen().onkeypress(back,'Down')
 turtle.Screen().onkeypress(right,'Right')
 turtle.Screen().onkeypress(left,'Left')
 
