7def8d7ff51dddec1639399a934f3757dc550e4c
TMarwah <44328801+TMarwah@users.noreply.github.com>
2020-05-10 18:19:08 -0700

    modified angles, screen size, comments

--- turtle_game.py
+++ turtle_game.py
@@ -1,28 +1,31 @@
+#import turtle package
 import turtle
-
 from turtle import Turtle, Screen
+#screen setup
 screen = Screen()
 screen.setup(500,500)
+#move forward
 def up():
     turtle.forward(100)
-
+#move backward
 def back():
     turtle.backward(100)
-
+#turn left
 def left():
     turtle.left(40)
-
+#turn right
 def right():
     turtle.right(40)
 
 
-
+#handle keypresses
 turtle.Screen().onkeypress(up,'Up')
 turtle.Screen().onkeypress(back,'Down')
 turtle.Screen().onkeypress(right,'Right')
 turtle.Screen().onkeypress(left,'Left')
-
+#listen for key input
 screen.listen()
 turtle.listen()
+#keep running program till exited
 turtle.mainloop()
 
